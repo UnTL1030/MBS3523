16a173596cff794461aa2020f7596f65442e12c6
UnTL1030 <78404072+UnTL1030@users.noreply.github.com>
2021-03-16 15:48:02 +0800

    Add files via upload

--- Assignment 1/MBS3523-Asn1-Q3.py
+++ Assignment 1/MBS3523-Asn1-Q3.py
@@ -2,23 +2,29 @@ import cv2
 import time
 import numpy as np
 import imutils
+import random
 
-cap = cv2.VideoCapture('rec/Video.mp4')
+cap = cv2.VideoCapture('rec/Video3.mp4')
 
-car_cascade = cv2.CascadeClassifier('rec/cars.xml')
-face=cv2.CascadeClassifier('rec/fullbody.xml')
+car_cascade = cv2.CascadeClassifier('rec/car.xml')
+face=cv2.CascadeClassifier('rec/haarcascade_fullbody.xml')
 
 while True:
     ret, frames = cap.read()
     gray=cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
-    cars= car_cascade.detectMultiScale(gray, 1.4, 2)
-    hum= face.detectMultiScale(gray, 1.1, 4)
+    c = random.randint(0, 256)
+    d = random.randint(0, 256)
+    e = random.randint(0, 256)
+    f = random.randint(0, 256)
+    g = random.randint(0, 256)
+    cars= car_cascade.detectMultiScale(gray, 2.22, 3)
+    hum= face.detectMultiScale(gray, 1.07, 6)
     for (x, y, w, h) in hum:
-        cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(frames, (x, y), (x + w, y + h), (c, d, e), 3)
     for (x, y, w, h) in cars:
-        cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        cv2.rectangle(frames, (x, y), (x+w, y+h), (e, f, g), 3)
         cv2.imshow('Detection', frames)
-    if cv2.waitKey(100) == 27:
+    if cv2.waitKey(1) == 0:
         break
 cap.release()
 cv2.destroyAllWindows()
